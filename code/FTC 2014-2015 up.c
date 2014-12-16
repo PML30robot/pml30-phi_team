0435a5bdabb0dee1386af419ca477f65e51ad84c
jenamax <jenamax@mail.ru>
2014-12-16 08:46:24 +0300

    Improved autonomus from the ramp

--- code/FTC 2014-2015 up.c
+++ code/FTC 2014-2015 up.c
@@ -1,5 +1,6 @@
 #pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTMotor,  HTMotor)
 #pragma config(Hubs,  S2, HTServo,  none,     none,     none)
+#pragma config(Sensor, S3,     color,          sensorCOLORRED)
 #pragma config(Motor,  mtr_S1_C1_1,     FR,            tmotorTetrix, openLoop, encoder)
 #pragma config(Motor,  mtr_S1_C1_2,     BR,            tmotorTetrix, openLoop, encoder)
 #pragma config(Motor,  mtr_S1_C2_1,     FL,            tmotorTetrix, openLoop, encoder)
@@ -22,9 +23,10 @@ int a = 64;
 
 void Servosetup()
 {
-		servo[servoClaws] = 235;
+	//	servo[servoClaws] = 235;
 	servo[servoBall] = 127;
-	servo[servoTube] = 152;
+	servo[servoTube] = 182;
+	servo[servoTube2] = 255 - ServoValue[servoTube];
 	servo[servoMvClaws] = 255;
 	nMotorEncoder[FR] = 0;
 	nMotorEncoder[FL] = 0;
@@ -39,15 +41,43 @@ void Servosetup()
 void motion(int l_ramp_goal, int a)
 {
 	int enc;
+	int pow = 100, color;
   nMotorEncoder[BR] = 0;
 	nMotorEncoder[BL] = 0;
-	while((a * (-nMotorEncoder[BR]) / 1000) * 3.14159265 * 10 < l_ramp_goal - 50)
+  motor[FR] = a * 100;
+  motor[BR] = a * 100;
+  motor[FL] = a * 100;
+  motor[BL] = a * 100;
+  wait1Msec(1500);
+  enc = -nMotorEncoder[BR];
+  nxtDisplayBigTextLine(2, "%i", enc);
+  if(-a * nMotorEncoder[BR] < 10)
+  {
+  	while(true)
+  	{
+  		for (; pow >= 0; pow--)
+  		{
+        motor[FR] = pow;
+        motor[BR] = pow;
+        motor[FL] = pow;
+        motor[BL] = pow;
+        wait1Msec(5);
+        enc = -nMotorEncoder[BR];
+        nxtDisplayBigTextLine(2, "%i", enc);
+      }
+  	}
+  }
+ /* if (SensorValue[S3] == color)
+  {
+  	  nMotorEncoder[BR] = 0;
+    	nMotorEncoder[BL] = 0;*/
+	while(-a * nMotorEncoder[BR] * 0.031415 < l_ramp_goal - 50)
 	{
 	  motor[FR] = a * 100;
     motor[BR] = a * 100;
     motor[FL] = a * 100;
     motor[BL] = a * 100;
-    enc = -nMotorEncoder[BR];
+    enc = nMotorEncoder[BR];
     nxtDisplayBigTextLine(2, "%i", enc);
   }
 
@@ -57,7 +87,7 @@ void motion(int l_ramp_goal, int a)
   motor[BL] = -100;
 //  wait1Msec(100);
 
-  while((a * (-nMotorEncoder[BR]) / 1000) * 3.14159265 * 10 < l_ramp_goal)
+  while(-a * nMotorEncoder[BR] * 0.031415 < l_ramp_goal)
 	{
 	  motor[FR] = a * 20;
     motor[BR] = a * 20;
@@ -65,12 +95,12 @@ void motion(int l_ramp_goal, int a)
     motor[BL] = a * 20;
   }
   servo[servoMvClaws] = 18;
-  while(( a * (-nMotorEncoder[BR]) / 1000) * 3.14159265 * 10 < l_ramp_goal + 18)
+  while(-a * nMotorEncoder[BR] * 0.031415 < l_ramp_goal + 20)
   {
-    motor[FR] = a * 10;
-    motor[BR] = a * 10;
-    motor[FL] = a * 10;
-    motor[BL] = a * 10;
+    motor[FR] = a * 20;
+    motor[BR] = a * 20;
+    motor[FL] = a * 20;
+    motor[BL] = a * 20;
 
   }
 
@@ -84,50 +114,73 @@ void elevator()
 {
 	int enc;
 	nMotorEncoder[UL] = 0;
-	while(nMotorEncoder[UL] < 12500)
+	motor[UR] = 100;
+  motor[UL] = -100;
+  motor[URT] = 100;
+  motor[ULT] = -100;
+  wait1Msec(1000);
+  if(-nMotorEncoder[UL] < 10)
+  {
+    while(true)
+    {
+  	  motor[UR] = 0;
+      motor[UL] = 0;
+      motor[URT] = 0;
+      motor[ULT] = 0;
+    }
+  }
+	while(-nMotorEncoder[UL] < 12000)
+	{
+    motor[UR] = 100;
+    motor[UL] = -100;
+    motor[URT] = 100;
+    motor[ULT] = -100;
+    enc = -nMotorEncoder[UL];
+    //nxtDisplayBigTextLine(1, "%i", enc);
+  }
+  motor[UR] = 0;
+  motor[UL] = 0;
+  motor[URT] = 0;
+  motor[ULT] = 0;
+
+ 	while(nMotorEncoder[UL] < -7300)
 	{
     motor[UR] = -100;
-    motor[UL] = 100;
-    servo[servoTube] = 152;
-    enc = nMotorEncoder[UL];
-    //nxtDisplayBigTextLine(1, "%i", enc);
-    if(nMotorEncoder[UL] > 5000)
-    	servo[servoBall] = 127;
+  	motor[UL] = 100;
+    motor[URT] = -100;
+    motor[ULT] = 100;
   }
+
   motor[UR] = 0;
   motor[UL] = 0;
-  servo[servoTube] = 152;
+  motor[URT] = 0;
+  motor[ULT] = 0;
 
- 	while(nMotorEncoder[UL] > 8500)
-	{
-    motor[UR] = 100;
-    motor[UL] = -100;
-    servo[servoTube] = 152;
-    servo[servoBall] = 127;
-  }
-  motor[UR] = 0;
-  motor[UL] = 0;
+  servo[servoTube] = 0;
+  servo[servoTube2] = 255;
+  wait1Msec(3500);
 
-  servo[servoTube] = 8;
-  wait1Msec(4000);
+  servo[servoTube] = 182;
+  servo[servoTube2] = 255 - ServoValue[servoTube];
 
-  servo[servoTube] = 152;
-  while(nMotorEncoder[UL] > 0)
+  /*while(nMotorEncoder[UL] < -10)
   {
-    motor[UR] = 100;
-    motor[UL] = -100;
-    servo[servoBall] = 127;
-  }
+    motor[UR] = -100;
+    motor[UL] = 100;
+    motor[URT] = -100;
+    motor[ULT] = 100;
+  }*/
 
   motor[UR] = 0;
   motor[UL] = 0;
-  servo[servoBall] = 255;
+  motor[URT] = 0;
+  motor[ULT] = 0;
 }
 
 void rotate(float l_ramp_goal)
 {
 	nMotorEncoder[BR] = 0;
-	while((-nMotorEncoder[BR] / 1000) * 3.14159265 * 10 < l_ramp_goal)
+	while(-nMotorEncoder[BR] /*/ 1000) * 3.14159265 * 10*/ < l_ramp_goal)
 	{
 	  motor[FR] = 100;
     motor[BR] = 100;
@@ -152,17 +205,16 @@ void rotate(float l_ramp_goal)
 
 task main()
 {
- waitForStart();
-
+	Servosetup();
+  waitForStart();
 	//StartTask(stage);
 	//StartTask(tube);
    servo[servoMvClaws] = 50;
-   servo[servoBall] = 255;
-   motion(310, 1);
+   motion(320, 1);
    elevator();
-   //servo[servoBall] = 127;
-   //rotate(0.1);
-   //motion(100, -1);
+   servo[servoBall] = 127;
+   rotate(550);
+   motion(385, -1);
 
 	while(true)
 	{
@@ -172,5 +224,7 @@ task main()
     motor[BL] = 0;
     motor[UR] = 0;
     motor[UL] = 0;
+    motor[URT] = 0;
+    motor[ULT] = 0;
   }
 }
